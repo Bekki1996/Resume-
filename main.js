5ad4898319b7bafd9468cc3cf5302193e5a345b9
Bekhzod Ismoilov <bekhzodismoilov@yahoo.com>
2021-01-27 22:10:06 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundClientReact().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+    console.log("timeElapsed: " + timeElapsed + "duration: " + duration);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+var arrow = document.querySelector(".arrow");
+
+arrow.addEventListener("click", function () {
+  smoothScroll(".arrow", 2000);
+});
